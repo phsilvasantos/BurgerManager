f7f69e4c03a3fe687fbaeeb3e976332a1dc4df07
Rodrigo Araujo <rodemfa@rodemfa-PC>
2019-10-28 18:44:11 -0300

    Definição da interface Action

--- BurgerManager/src/burger/BurgerMan.java
+++ BurgerManager/src/burger/BurgerMan.java
@@ -1,7 +1,36 @@
 package burger;
 
+import burger.action.*;
+
+import java.util.Scanner;
+
 public class BurgerMan {
+   public static final Scanner input = new Scanner(System.in);
+
    public static void main(String[] args) {
-      System.out.println("Em desenvolvimento.");
+      Action[] actions = new Action[] {};
+
+      while (true) {
+         System.out.println("---\n0 - sair");
+         int a;
+         for (a = 1; a <= actions.length; a++)
+            System.out.println(a + " - " + actions[a - 1]);
+         System.out.print("---\nAção: ");
+
+         try {
+            a = Integer.parseInt(input.nextLine());
+            if (a == 0)
+               break;
+
+            actions[a - 1].execute();
+         }
+
+         catch (NumberFormatException | IndexOutOfBoundsException ex) {
+            System.out.println("<!> Entrada inválida.");
+         }
+         catch (Exception ex) {
+            System.out.println("<!> " + ex.getMessage());
+         }
+      }
    }
 }
---
+++ BurgerManager/src/burger/action/Action.java
@@ -0,0 +1,5 @@
+package burger.action;
+
+public interface Action {
+   public void execute() throws Exception;
+}
